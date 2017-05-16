06f0961bc3c6de5f7a36f351d7f1c729ac50626b
bakslash <marcusmuasa@gmail.com>
2017-05-16 16:24:09 +0300

    api intergration

--- q_app/app/views.py
+++ q_app/app/views.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse
+import requests
+import json
 
 # Create your views here.
 
@@ -9,3 +11,20 @@ def index(request):
     return HttpResponse('Hello World')
 def test(request):
     return HttpResponse('My second view!')
+def profile(request):
+    jsonList = []
+    req = requests.get('https://api.github.com/users/bakslash')
+    jsonList.append(json.loads(req.content))
+    parsedData = []
+    userData = {}
+    for data in jsonList:
+        userData['name'] = data['name']
+        userData['blog'] = data['blog']
+        userData['email'] = data['email']
+        userData['public_gists'] = data['public_gists']
+        userData['public_repos'] = data['public_repos']
+        userData['avatar_url'] = data['avatar_url']
+        userData['followers'] = data['followers']
+        userData['following'] = data['following']
+    parsedData.append(userData)
+    return HttpResponse(parsedData)
